7da51b2846a0ec492cd7097a2fbdb3246c924692
Anibalgomezfer <59883735+Anibalgomezfer@users.noreply.github.com>
2020-11-27 12:38:43 +0100

    Todo ok

--- src/main/java/com/clases/PSP_FINAL_1T_AGF_2/Controlador.java
+++ src/main/java/com/clases/PSP_FINAL_1T_AGF_2/Controlador.java
@@ -2,17 +2,18 @@ package com.clases.PSP_FINAL_1T_AGF_2;
 
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.concurrent.Semaphore;
 
 public class Controlador {
     public static void main(String[] args) {
         int numRegistros = 0;
         int numHilos = 5;
-        int ingresosBBDD = 0;
+        final int ingresosBBDD = 0;
+
 
         ArrayList<Empleado> arrayEmpleados = new ArrayList<>();
-        leerRegistrosHilos(numRegistros, numHilos, ingresosBBDD, arrayEmpleados);
         leerRegistrosSecuencial(numRegistros, ingresosBBDD);
-
+        leerRegistrosHilos(numRegistros, numHilos, ingresosBBDD, arrayEmpleados);
 
     }
 
@@ -23,11 +24,9 @@ public class Controlador {
         try {
             Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost/bbdd_psp_1", "DAM2020_PSP", "DAM2020_PSP");
             Statement consulta = conexion.createStatement();
-            ResultSet resultSet = consulta.executeQuery("SELECT COUNT(*) from empleados");
-            while (resultSet.next()) {
-                numRegistros = resultSet.getInt(1);
-            }
-
+            ResultSet resultSet = consulta.executeQuery("SELECT COUNT(ID) from empleados");
+            resultSet.next();
+            numRegistros = resultSet.getInt(1);
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
@@ -38,19 +37,28 @@ public class Controlador {
         for (int indice = 0; indice < numHilos; indice++) {
             if (indice == (numHilos-1)){
                 numRegistrosPorHilo = numRegistrosPorHilo+modulo;
-                Empleado hilo = new Empleado(numRegistros, numHilos, numRegistrosPorHilo, indice, modulo);
+                Empleado hilo = new Empleado(numRegistrosPorHilo, indice);
                 arrayEmpleados.add(hilo);
             }else{
                 modulo=0;
-                Empleado hilo = new Empleado(numRegistros, numHilos, numRegistrosPorHilo, indice, modulo);
+                Empleado hilo = new Empleado(numRegistrosPorHilo, indice);
                 arrayEmpleados.add(hilo);
             }
         }
 
-        //Tengo que sincronizar los hilos (????????????) porque si no, no suma bien los salarios
-
         for (int j = 0; j < arrayEmpleados.size(); j++) {
             arrayEmpleados.get(j).start();
+        }
+
+        arrayEmpleados.forEach(empleado -> {
+            try {
+                empleado.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        for (int j = 0; j < arrayEmpleados.size(); j++) {
             ingresosBBDD += arrayEmpleados.get(j).getIngresosBBDD();
         }
 
--- src/main/java/com/clases/PSP_FINAL_1T_AGF_2/Empleado.java
+++ src/main/java/com/clases/PSP_FINAL_1T_AGF_2/Empleado.java
@@ -1,20 +1,12 @@
 package com.clases.PSP_FINAL_1T_AGF_2;
-
-import com.github.javafaker.Faker;
-
 import java.sql.*;
 
 public class Empleado extends Thread {
 
-    private int id;
-    private String email;
-    private int ingresos;
-    int numRegistros;
-    int numHilos;
     int numRegistrosPorHilo;
     int indice;
     int ingresosBBDD;
-    int modulo;
+
 
     public int getIngresosBBDD() {
         return ingresosBBDD;
@@ -24,12 +16,9 @@ public class Empleado extends Thread {
         this.ingresosBBDD = ingresosBBDD;
     }
 
-    public Empleado(int numRegistros, int numHilos, int numRegistrosPorHilo, int indice, int modulo) {
-        this.numRegistros = numRegistros;
-        this.numHilos = numHilos;
+    public Empleado(int numRegistrosPorHilo, int indice) {
         this.numRegistrosPorHilo = numRegistrosPorHilo;
         this.indice = indice;
-        this.modulo = modulo;
     }
 
     @Override
@@ -40,21 +29,21 @@ public class Empleado extends Thread {
 
 
     public void leerDatos(int indice, int ingresosBBDD) {
-        try {
-            for (int j = (numRegistrosPorHilo * indice) + 1; j <= numRegistrosPorHilo * (indice + 1); j++) {
-                ingresos = (int) (Math.random() * 990) + 10;
-                email = Faker.instance().internet().emailAddress();
-                Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost/bbdd_psp_1", "DAM2020_PSP", "DAM2020_PSP");
-                Statement consulta = conexion.createStatement();
-                ResultSet resultSet = consulta.executeQuery("select * from empleados where ID=" + j);
-                while (resultSet.next()){
+        synchronized (this) {
+            try {
+                for (int j = (numRegistrosPorHilo * indice) + 1; j <= numRegistrosPorHilo * (indice + 1); j++) {
+                    Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost/bbdd_psp_1", "DAM2020_PSP", "DAM2020_PSP");
+                    Statement consulta = conexion.createStatement();
+                    ResultSet resultSet = consulta.executeQuery("select * from empleados where ID=" + j);
+                    resultSet.next();
                     ingresosBBDD += resultSet.getInt(3);
+                    conexion.close();
                 }
-                conexion.close();
+                setIngresosBBDD(ingresosBBDD);
+            } catch (SQLException e) {
+                e.printStackTrace();
+                System.out.println("La conexion a la base de datos ha fallado. \nAsegurate de que tienes encendido xampp y los modulos de Apache y MySQL.");
             }
-        } catch (SQLException e) {
-            System.out.println("La conexion a la base de datos ha fallado. \nAsegurate de que tienes encendido xampp y los modulos de Apache y MySQL.");
         }
-        setIngresosBBDD(ingresosBBDD);
     }
 }
